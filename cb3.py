5d18be2cd2ba87d350ff393259c3ff4e0dbb6d7d
Hanshia <73780714+Hanshia@users.noreply.github.com>
2025-02-11 17:44:17 +0900

    Update cb3.py

--- cb3.py
+++ cb3.py
@@ -287,6 +287,7 @@ if st.session_state.stage == 1:
 
 # 대화 진행
 elif st.session_state.stage == 2:
+    input_container = st.empty()
     user_input = st.chat_input("대화를 입력하세요:", key="input_conversation")
     if user_input:
         st.session_state.messages.append({"role": "user", "content": user_input})
@@ -294,7 +295,6 @@ elif st.session_state.stage == 2:
         # UI 업데이트 (사용자 메시지 추가)
         with chat_container.container():
             display_chat_message("user", user_input, user_avatar_url)
-            st.rerun()
 
         with st.spinner('답변 생성 중... 잠시만 기다려 주세요.'):
             response = get_response(st.session_state.character, user_input)
