ac08404ada489c2fd473d38768b8aff4b317bfbb
Hanshia <73780714+Hanshia@users.noreply.github.com>
2025-02-11 18:04:36 +0900

    Update cb3.py

--- cb3.py
+++ cb3.py
@@ -291,21 +291,15 @@ if st.session_state.stage == 1:
 
 # 대화 진행
 elif st.session_state.stage == 2:
+    input_container = st.empty()
     user_input = st.chat_input("대화를 입력하세요:", key="input_conversation")
     if user_input:
         st.session_state.messages.append({"role": "user", "content": user_input})
-
-    # UI 업데이트 (사용자 메시지 추가)
-    with chat_container.container():
-        display_chat_message("user", user_input, user_avatar_url)
-
-    with st.spinner('답변 생성 중... 잠시만 기다려 주세요.'):
-        response = get_response(st.session_state.character, user_input)
-        st.session_state.messages.append({"role": "assistant", "content": response})
-
-        # UI 업데이트 (챗봇 응답 추가)
-        with chat_container.container():
-            display_chat_message("assistant", response, st.session_state.character_avatar_url)
+        st.rerun()
+    if st.session_state.messages and st.session_state.messages[-1]["role"] == "user":
+        with st.spinner('답변 생성 중... 잠시만 기다려 주세요.'):
+            response = get_response(st.session_state.character, user_input)
+            st.session_state.messages.append({"role": "assistant", "content": response})
 
 chat_container.empty()  # 이전 메시지 지우기
 with chat_container.container():
